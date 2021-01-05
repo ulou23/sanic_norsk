af4320f0a3f6e512c93b2a73b1820ee94fb101e6
ullu <ula.lukierska@gmail.com>
2021-01-05 01:08:23 +0100

    request method

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ import logging
 
 from models import Users
 from sanic import Sanic,response
+from sanic.log import logger
 from tortoise.contrib.sanic import register_tortoise
 
 logging.basicConfig(level=logging.DEBUG)
 
 app=Sanic(__name__)
 
-@app.route("/")
-async def list_all(request):
-    users=await Users.all()
-    return response.json({'users': [str(user) for user in users]})
+@app.route('/')
+async def req_get(request):
+    return json({
+        "parsed":True,
+        "url": request.url,
+        "query_string":request.query_string,
+        "args": request.args,
+        "query_args":request.query_args,
+        "endpoint":request.endpoint
+    })
 
-@app.route("/user")
-async def add_user(request):
-    user=await Users.create(name="new")
-    return response.json({'user':str(user)})
+@app.route('file/')
+async def post_file(request):
+    return json({"received": True, "file_names": request.method, "params":request.host})
 
-register_tortoise(
-    app, db_url="sqlite://:memory",modules={"models": ["models"]},generate_schemas=True
-)
+#@app.route("/users")
+#async def list_all(request):
+#    users=await Users.all()
+#    logger.info('yes test.py log')                #first request info view
+#    return response.json({'users': [str(user) for user in users]})
+#
+#@app.route("/user")
+#async def add_user(request):
+#    user=await Users.create(name="new")
+#    return response.json({'user':str(user)})
+#
+#register_tortoise(
+#    app, db_url="sqlite://:memory",modules={"models": ["models"]},generate_schemas=True
+#)
 
 
 if __name__=="__main__":
-    app.run(port=5000)
+    app.run(port=5000, access_log=True)
